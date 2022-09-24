7f507c988e6e77fbf0579f3b60511c51689ba0f6
AineeJames <olsenaiden33@gmail.com>
2022-09-24 15:37:00 -0700

    scre mouse light control

--- Godot/Light2D.gd
+++ Godot/Light2D.gd
@@ -8,8 +8,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	var vec = get_global_mouse_position() # getting the vector from self to the mouset
-	position = vec # move by that vector
+	pass
+#	var vec = get_global_mouse_position() # getting the vector from self to the mouset
+#	position = vec # move by that vector
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
