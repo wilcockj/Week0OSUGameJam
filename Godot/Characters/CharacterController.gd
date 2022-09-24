9a51f0cb2dd3601d4f032af4ba48da88e145a38b
James Wilcock <jameswilcock8@gmail.com>
2022-09-24 12:42:40 -0700

    added extensible epic characters

--- Godot/Characters/CharacterController.gd
+++ Godot/Characters/CharacterController.gd
@@ -1,24 +1,43 @@
+class_name Character
 extends KinematicBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
-const JUMP_SPEED = 100.0
+export var GRAVITY = 200.0
+export var WALK_SPEED = 200
+export var JUMP_SPEED = 100.0
 
 var velocity = Vector2()
-
+var selection_index = 1 setget set_selection, get_selection
 func _physics_process(delta):
+	if selection_index == Global.selected:
+		
+
+		if Input.is_action_pressed("ui_left"):
+			velocity.x = -WALK_SPEED
+		elif Input.is_action_pressed("ui_right"):
+			velocity.x =  WALK_SPEED
+		else:
+			velocity.x = 0
+		if Input.is_action_just_pressed("jump"):
+			velocity.y -= JUMP_SPEED
+		# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 	velocity.y += delta * GRAVITY
-
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
-	else:
-		velocity.x = 0
-	if Input.is_action_just_pressed("jump"):
-		velocity.y -= JUMP_SPEED
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+		# The second parameter of "move_and_slide" is the normal pointing up.
+		# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	move_and_slide(velocity, Vector2(0, -1))
+	if Input.is_action_just_pressed("select_1"):
+		Global.selected = 1
+	elif Input.is_action_just_pressed("select_2"):
+		Global.selected = 2
+	elif Input.is_action_just_pressed("select_3"):
+		Global.selected = 3
+	elif Input.is_action_just_pressed("select_4"):
+		Global.selected = 4
+	elif Input.is_action_just_pressed("select_5"):
+		Global.selected = 5
+	elif Input.is_action_just_pressed("select_6"):
+		Global.selected = 6
+
+func get_selection():
+	return selection_index
+func set_selection(var new_selection) -> void:
+	selection_index = new_selection
---
+++ Godot/Characters/Jumper.gd
@@ -0,0 +1,5 @@
+extends Character
+
+var idx = 2
+func _ready():
+	set_selection(2)
