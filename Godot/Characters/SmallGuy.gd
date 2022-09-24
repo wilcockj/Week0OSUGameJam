e7b6558cd7c39302765ae2f75a6869d2fb3125c6
James Wilcock <jameswilcock8@gmail.com>
2022-09-24 13:45:57 -0700

    added camera to all characters

--- Godot/Characters/SmallGuy.gd
+++ Godot/Characters/SmallGuy.gd
@@ -1,5 +1,11 @@
 extends Character
 
-var idx = 3
+var idx = 2
 func _ready():
 	set_selection(idx)
+
+func _process(delta):
+	if Global.selected == idx:
+		$Camera2D.current = true
+	else:
+		$Camera2D.current = false
