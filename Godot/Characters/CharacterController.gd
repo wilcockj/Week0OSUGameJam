433ad0b152a1a11bcd3fcd3e8260e79172121ac2
James Wilcock <jameswilcock8@gmail.com>
2022-09-24 13:11:03 -0700

    fixed gravity when on floor

--- Godot/Characters/CharacterController.gd
+++ Godot/Characters/CharacterController.gd
@@ -8,6 +8,10 @@ export var JUMP_SPEED = 100.0
 var velocity = Vector2()
 var selection_index = 1 setget set_selection, get_selection
 func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += delta * GRAVITY
+	else:
+		velocity.y = 0
 	if selection_index == Global.selected:
 		
 
@@ -17,11 +21,10 @@ func _physics_process(delta):
 			velocity.x =  WALK_SPEED
 		else:
 			velocity.x = 0
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y -= JUMP_SPEED
 		# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-	if !is_on_floor():
-		velocity.y += delta * GRAVITY
+	
 		# The second parameter of "move_and_slide" is the normal pointing up.
 		# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	move_and_slide(velocity, Vector2(0, -1))
