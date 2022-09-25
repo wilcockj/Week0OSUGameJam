1018004c7cc5da312accd338559856ea2c9d3806
James Wilcock <jameswilcock8@gmail.com>
2022-09-24 23:28:43 -0700

    added bodge for multiple water pieces

--- Godot/Level2.gd
+++ Godot/Level2.gd
@@ -7,7 +7,7 @@ extends "res://Level1.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.can_be_selected = [1,2]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
